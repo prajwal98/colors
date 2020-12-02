a12b1551331260424959ba57e86d4179375a41f4
Dev <ursprajwal1@gmail.com>
2020-12-02 13:59:23 +0530

    Updates

---
+++ precache-manifest.1605a0a0c059d8af8853b2b7e3459952.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c11f5a262a31c5c2ee76",
+    "url": "/colors/static/css/main.5cf60200.chunk.css"
+  },
+  {
+    "revision": "c11f5a262a31c5c2ee76",
+    "url": "/colors/static/js/main.ac52c7ca.chunk.js"
+  },
+  {
+    "revision": "4833ddfbf90387bb4bc8",
+    "url": "/colors/static/js/runtime~main.febe43e6.js"
+  },
+  {
+    "revision": "850ed519f91fd6d966d0",
+    "url": "/colors/static/css/2.0541f974.chunk.css"
+  },
+  {
+    "revision": "850ed519f91fd6d966d0",
+    "url": "/colors/static/js/2.ebd8bb4f.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/colors/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "5a4d2bc6ee211f35758c5db34611559a",
+    "url": "/colors/index.html"
+  }
+];
